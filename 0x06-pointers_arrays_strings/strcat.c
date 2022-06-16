df731b3d6c0f727a6ca1d0a5953a75efc56e4979
Obinna onyia <stanleyobinna62@gmail.com>
2022-06-16 16:16:54 -0700

    concatenation

--- 0x06-pointers_arrays_strings/strcat.c
+++ 0x06-pointers_arrays_strings/strcat.c
@@ -6,14 +6,8 @@
  *  return: a string
  *
  */
-char *_strcat(char *dest, char *src){
-	int len =0, i ;
-	while (dest[len])
-		len++;
-	for (i=0;src[i]!='\0'; i++){
-		dest[len] = src[i];
-		len += 1;
-	}
-	dest[len] = '\0';
+char *_strcat(char *dest, char *src, int n){
+	strncat(dest, src, n);
+	
 	return (dest);
 }

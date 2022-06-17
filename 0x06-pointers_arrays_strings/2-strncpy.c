cd4eed853265ba56a7a67f2c98bc889df52969e7
Obinna onyia <stanleyobinna62@gmail.com>
2022-06-16 18:01:26 -0700

    hot fix

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/*
+ *_strncpy - for copying purposes
+ *@dest: destination string
+ *@src: source string
+ *
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}

2ae5c288d83a9af515f5fb6849afe85b8792003c
Obinna onyia <stanleyobinna62@gmail.com>
2022-06-16 18:33:57 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcmp - compare strings
+ *s1 : string 1
+ *s2 : string 2
+ *return: integer
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+	result = strcmp(s1, s2);
+	return (result);
+}
